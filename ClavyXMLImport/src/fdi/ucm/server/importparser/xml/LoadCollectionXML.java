47f9988044bd36efcd7ce071147f4dfa446fdeaf
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2018-01-25 15:04:55 +0100

    go go

--- ClavyXMLImport/src/fdi/ucm/server/importparser/xml/LoadCollectionXML.java
+++ ClavyXMLImport/src/fdi/ucm/server/importparser/xml/LoadCollectionXML.java
@@ -1,12 +1,13 @@
 /**
  * 
  */
-package fdi.ucm.server.importparser.medpix;
+package fdi.ucm.server.importparser.xml;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -22,15 +23,18 @@ import fdi.ucm.server.modelComplete.LoadCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteCollectionAndLog;
 import fdi.ucm.server.modelComplete.collection.document.CompleteDocuments;
+import fdi.ucm.server.modelComplete.collection.document.CompleteLinkElement;
 import fdi.ucm.server.modelComplete.collection.document.CompleteTextElement;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteElementType;
 import fdi.ucm.server.modelComplete.collection.grammar.CompleteGrammar;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteLinkElementType;
 import fdi.ucm.server.modelComplete.collection.grammar.CompleteTextElementType;
 
 /**
  * @author Joaquin Gayoso Cabada
  *
  */
-public class LoadCollectionMedPix extends LoadCollection{
+public class LoadCollectionXML extends LoadCollection{
 
 	
 	private static ArrayList<ImportExportPair> Parametros;
@@ -44,8 +48,8 @@ public class LoadCollectionMedPix extends LoadCollection{
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		LoadCollectionMedPix LC=new LoadCollectionMedPix();
-		LoadCollectionMedPix.consoleDebug=true;
+		LoadCollectionXML LC=new LoadCollectionXML();
+		LoadCollectionXML.consoleDebug=true;
 		
 		ArrayList<String> AA=new ArrayList<String>();
 		AA.add("Ejemplo");
@@ -77,7 +81,12 @@ public class LoadCollectionMedPix extends LoadCollection{
 	public CompleteCollectionAndLog processCollecccion(ArrayList<String> dateEntrada) {
 		try {
 			
-			HashMap<CompleteGrammar,HashMap<String,CompleteTextElementType>> ListaElem=new HashMap<CompleteGrammar,HashMap<String,CompleteTextElementType>>();
+			HashMap<String,CompleteDocuments> tablaEqui=new HashMap<String,CompleteDocuments>();
+			HashMap<CompleteLinkElement, String> tablaLink=new HashMap<CompleteLinkElement,String>();
+			HashMap<CompleteLinkElement, CompleteDocuments> tablaLinkPadre=new HashMap<CompleteLinkElement,CompleteDocuments>();
+			List<CompleteLinkElement> posProcessLink=new ArrayList<CompleteLinkElement>();
+			
+			HashMap<CompleteGrammar,HashMap<String,CompleteElementType>> ListaElem=new HashMap<CompleteGrammar,HashMap<String,CompleteElementType>>();
 			HashMap<String,CompleteGrammar> ListaGram=new HashMap<String,CompleteGrammar>();
 			
 			CompleteCollectionAndLog Salida=new CompleteCollectionAndLog();
@@ -105,10 +114,15 @@ public class LoadCollectionMedPix extends LoadCollection{
 					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 						
 						Element eElement = (Element) nNode;
+						
+						
 						String descript = eElement.getElementsByTagName("description").item(0).getTextContent();
 						String icon = eElement.getElementsByTagName("icon").item(0).getTextContent();
 						CompleteDocuments CD=new CompleteDocuments(CC, descript, icon);
-						
+						String Id=eElement.getAttribute("id");
+						if (!(Id.isEmpty()||Id==null))
+							tablaEqui.put(Id, CD);
+	
 						CC.getEstructuras().add(CD);
 						
 						try {
@@ -137,30 +151,52 @@ public class LoadCollectionMedPix extends LoadCollection{
 												Grama=new CompleteGrammar(GA, GA, CC);
 												CC.getMetamodelGrammar().add(Grama);
 												ListaGram.put(GA, Grama);
-												ListaElem.put(Grama, new HashMap<String,CompleteTextElementType>());
+												ListaElem.put(Grama, new HashMap<String,CompleteElementType>());
 												Salida.getLogLines().add("Creada la Gramatica-> " +GA);
 												}
 												
 											String ElemTy=eElement2.getTagName();
 											
-											HashMap<String, CompleteTextElementType> ListaElemTy = ListaElem.get(Grama);
-											CompleteTextElementType ElementoTy=ListaElemTy.get(ElemTy);
+											HashMap<String, CompleteElementType> ListaElemTy = ListaElem.get(Grama);
+											CompleteElementType ElementoTy=ListaElemTy.get(ElemTy);
 											
 											if (ElementoTy==null)
 											{
-											ElementoTy=new CompleteTextElementType(ElemTy, Grama);
-											Grama.getSons().add(ElementoTy);
-											ListaElemTy.put(ElemTy, ElementoTy);
-											ListaElem.put(Grama, ListaElemTy);
-											Salida.getLogLines().add("Creada la Gramatica-> " +ElemTy);
+											
+												String rara=eElement2.getAttribute("relation");	
+												if (rara!=null&&rara.equals("true"))
+												{
+													ElementoTy=new CompleteLinkElementType(ElemTy, Grama);
+													Grama.getSons().add(ElementoTy);
+													ListaElemTy.put(ElemTy, ElementoTy);
+													ListaElem.put(Grama, ListaElemTy);
+													Salida.getLogLines().add("Creada la Gramatica-> " +ElemTy);
+												}
+												else
+												{	
+												ElementoTy=new CompleteTextElementType(ElemTy, Grama);
+												Grama.getSons().add(ElementoTy);
+												ListaElemTy.put(ElemTy, ElementoTy);
+												ListaElem.put(Grama, ListaElemTy);
+												Salida.getLogLines().add("Creada la Gramatica-> " +ElemTy);
+												}
 											}
 											
 											String Valor=eElement2.getTextContent();
 											
-											CompleteTextElement EE=new CompleteTextElement(ElementoTy,Valor);
+											if (ElementoTy instanceof CompleteTextElementType)
+											{
+											CompleteTextElement EE=new CompleteTextElement((CompleteTextElementType) ElementoTy,Valor);
 											CD.getDescription().add(EE);
-											
-											
+											}else
+												if (ElementoTy instanceof CompleteLinkElementType)
+												{
+													CompleteLinkElement EE=new CompleteLinkElement((CompleteLinkElementType) ElementoTy,null);
+													CD.getDescription().add(EE);
+													posProcessLink.add(EE);
+													tablaLink.put(EE, Valor);
+													tablaLinkPadre.put(EE, CD);
+												}
 												}
 										
 										
@@ -204,6 +240,16 @@ public class LoadCollectionMedPix extends LoadCollection{
 //			ProcesaCasoID();
 //			ProcesaTopics();
 //			//AQUI se puede trabajar
+			
+			for (CompleteLinkElement completeLinkElement : posProcessLink) {
+				String valor = tablaLink.get(completeLinkElement);
+				CompleteDocuments dd=tablaEqui.get(valor);
+				if (dd==null)
+				tablaLinkPadre.get(completeLinkElement).getDescription().remove(completeLinkElement);
+				else	
+				completeLinkElement.setValue(dd);
+			}
+			
 			
 			
 			return Salida;
